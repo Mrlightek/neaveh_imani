04b15f7da50f16377db42cad6b411f31b93c99a3
Marlon Henry <haveyec@me.com>
2024-09-02 11:03:46 -0400

    Hope this gets the generators registered like I would want

--- lib/nevaeh.rb
+++ lib/nevaeh.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "nevaeh/version"
+require "generators/nevaeh/install_generator"
+#require "generators/nevaeh/init_generator"
 
 module Nevaeh
   class Error < StandardError; end
